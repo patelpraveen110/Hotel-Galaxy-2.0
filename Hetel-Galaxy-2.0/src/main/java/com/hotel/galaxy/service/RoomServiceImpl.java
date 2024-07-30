97df8faf54d8d17e19c8f6a54f3d42ac4300da25
patelpraveen110 <patelpraveen110@gmail.com>
2024-07-30 10:19:55 +0530

    Adding New Room Feature backend code added

--- Hetel-Galaxy-2.0/src/main/java/com/hotel/galaxy/service/RoomServiceImpl.java
+++ Hetel-Galaxy-2.0/src/main/java/com/hotel/galaxy/service/RoomServiceImpl.java
@@ -1,4 +1,33 @@
 package com.hotel.galaxy.service;
 
-public class RoomServiceImpl {
+import com.hotel.galaxy.model.Room;
+import com.hotel.galaxy.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements RoomService {
+
+    private final RoomRepository roomRepository;
+    @Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice)
+            throws SQLException, IOException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if(!file.isEmpty()){
+            byte[] photoByte = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoByte);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
 }
