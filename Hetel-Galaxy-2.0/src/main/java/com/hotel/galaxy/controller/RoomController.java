c4c7058c35105d6e4ca6cce01fbf29b0b4469c94
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-01 00:50:39 +0530

    add room functionality added

--- Hetel-Galaxy-2.0/src/main/java/com/hotel/galaxy/controller/RoomController.java
+++ Hetel-Galaxy-2.0/src/main/java/com/hotel/galaxy/controller/RoomController.java
@@ -7,17 +7,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/rooms")
+@CrossOrigin(origins = "http://localhost:5174")
 @RestController
 public class RoomController {
     private final RoomService roomService;
-
     @PostMapping("/add/new-room")
     public ResponseEntity<RoomResponse> addNewRoom(
             @RequestParam("photo") MultipartFile photo,
@@ -28,6 +28,28 @@ public class RoomController {
         RoomResponse roomResponse = new RoomResponse(savedRoom.getId(),
                 savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(roomResponse);
-
     }
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes(){
+        return roomService.getAllRomTypes();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
