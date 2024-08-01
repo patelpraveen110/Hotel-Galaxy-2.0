cb34af7c1c6135fa22f4c8c14c24f9d8a2923875
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-01 23:58:43 +0530

    add functionality

--- hotel-galaxy-ui/src/main.jsx
+++ hotel-galaxy-ui/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      {" "}
+      {/* Wrap App component with Router */}
+      <App />
+    </Router>
+  </React.StrictMode>
+);
